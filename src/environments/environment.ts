d965ed3c047026f9087b0565cf84a3df006d5ab2
NishiGaba <gabanishi45@gmail.com>
2021-01-16 17:35:37 +0530

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: true,
   firebase : {
-    apiKey: "AIzaSyB_9X_kC9swFvbJF2-ii3so4HJ-Fp7MWYI",
-    authDomain: "angular30-25eb3.firebaseapp.com",
-    projectId: "angular30-25eb3",
-    storageBucket: "angular30-25eb3.appspot.com",
-    messagingSenderId: "316986160259",
-    appId: "1:316986160259:web:cb5be43fecd785aa9a97b6"
+    apiKey: "***",
+    authDomain: "***.firebaseapp.com",
+    projectId: "***",
+    storageBucket: "***.appspot.com",
+    messagingSenderId: "***",
+    appId: "1:***"
   }
 };
 
